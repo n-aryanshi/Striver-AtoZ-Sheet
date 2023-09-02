df03038bcdf74e33d3cff4f1eaedc88f1dd57a66
Aryanshi Naglaksh <80567421+n-aryanshi@users.noreply.github.com>
2023-09-02 22:09:59 +0530

    Update gcdOrHcf.java

--- Basics Programming/gcdOrHcf.java
+++ Basics Programming/gcdOrHcf.java
@@ -1,1 +1,23 @@
+public class Solution {
+    public static int calcGCD(int n, int m){
+        // Write your code here.
 
+        int result =-1;
+        while(n>0&&m>0){
+            if(n>m){
+                n=n%m;
+            }
+            else{
+                m=m%n;
+            }
+        }
+        if(m==0){
+            result = n;
+        }
+        if(n==0){
+            result = m;
+        }
+
+        return result;
+        }
+    }
