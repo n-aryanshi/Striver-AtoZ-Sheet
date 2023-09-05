58693d11d06c0ea29190997a2c07a18d06bf1bc5
Aryanshi Naglaksh <80567421+n-aryanshi@users.noreply.github.com>
2023-09-05 18:56:08 +0530

    Update largestElementIntheArray.java

--- Array_problems_easy/largestElementIntheArray.java
+++ Array_problems_easy/largestElementIntheArray.java
@@ -5,7 +5,7 @@ public class Solution {
 
     static int largestElement(int[] arr, int n) {
         // Write your code here.
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for(int i=0; i<n; i++)
         {
             if(arr[i]>max)
